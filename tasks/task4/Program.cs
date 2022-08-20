a94091d70951f2c08d222f9e467bc241a70a9ec7
Dima <markovda7@gmail.com>
2022-08-20 12:52:30 +0300

    add task4

---
+++ tasks/task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (max < b) max = b;
+
+if (max < c) max = c;
+
+Console.WriteLine(max);
