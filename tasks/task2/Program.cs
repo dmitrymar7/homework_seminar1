8cb1b55963366d3806acc061cfee51514cbf8bbf
Dima <markovda7@gmail.com>
2022-08-20 12:48:00 +0300

    add task2

---
+++ tasks/task2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (max < b)
+{
+    max = b;
+}
+Console.WriteLine("max = " + max);
