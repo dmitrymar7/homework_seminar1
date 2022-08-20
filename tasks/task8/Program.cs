033801c1281b3cb1ec26eac17080f5724dcc88c1
Dima <markovda7@gmail.com>
2022-08-20 12:59:22 +0300

    add task8

---
+++ tasks/task8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int start = 1;
+
+while (Math.Abs(start) <= Math.Abs(a)){
+    if (start % 2 == 0){
+        Console.WriteLine(start);    
+    }
+
+    if (a > 0) start++;
+    else start--;
+     
+}
